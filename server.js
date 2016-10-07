3eb3aa2472dc8f038d6b3202099c0f1954e26ed3
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-10-07 13:16:40 +1100

    init

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-MongoClient.connect('mongodb://lelz:lolz@ds015995.mlab.com:15995/cruddly', (err, database) => {
+MongoClient.connect('mongodb://localhost/trolleysystem', (err, database) => {
   if (err) return console.log(err);
   db = database;
   app.listen(app.get('port'), function() {
@@ -15,17 +15,9 @@ MongoClient.connect('mongodb://lelz:lolz@ds015995.mlab.com:15995/cruddly', (err,
   });
 });
 app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, result) => {
+  db.collection('b3').find().toArray((err, result) => {
     if (err) return console.log(err);
     // renders index.ejs
     res.render('index.ejs', {quotes: result});
-  });
-});
-app.post('/quotes', (req, res) => {
-  console.log(req.body);
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log('saved to db');
-    res.redirect('/');
   });
 });
