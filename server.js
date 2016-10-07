14e043e02953e88fec0fd58a12951c4fb2353bd7
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-10-07 14:36:59 +1100

    init

--- server.js
+++ server.js
@@ -14,10 +14,76 @@ MongoClient.connect('mongodb://localhost/trolleysystem', (err, database) => {
     console.log(`running on 0.0.0.0:${app.get('port')}`);
   });
 });
-app.get('/', (req, res) => {
-  db.collection('b3').find().toArray((err, result) => {
-    if (err) return console.log(err);
-    // renders index.ejs
-    res.render('index.ejs', {quotes: result});
+
+
+
+
+
+var collectionOne = 0;
+var collectionTwo = 0;
+var collectionThree = 0;
+app.get('/', function(req, res){
+      db.collection("b1", function(err, collection) {
+      collection.find({"out": "x"}).toArray(function(err, result) {
+        if (err) {
+          throw err;
+        } else {
+          for (i=0; i<result.length; i++) {
+          }
+          collectionOne = i;
+        }
+      });
+      db.collection("b2", function(err, collection) {
+        collection.find({"out": "x"}).toArray(function(err, result) {
+          if (err) {
+            throw err;
+          } else {
+            for (i=0; i<result.length; i++) {}
+            collectionTwo = i;
+          }
+        });
+      });
+      db.collection("b3", function(err, collection) {
+        collection.find({"out": "x"}).toArray(function(err, result) {
+          if (err) {
+            throw err;
+          } else {
+            for (i=0; i<result.length; i++) {}
+            collectionThree = i;
+          }
+        });
+      });
+      // Thank you aesed
+      function workyabastard() {console.log(collectionOne+":"+collectionTwo +":"+collectionThree);
+      res.render('index.ejs', {
+        bay: collectionOne,
+        bay2: collectionTwo,
+        bay3: collectionThree
+      });}
+            setTimeout(workyabastard, 3000);
+    });
   });
-});
+
+
+
+
+// app.get('/', (req, res) => {
+//
+//
+//   //db.listCollections().toArray(function(err, collInfos) {
+//       // collInfos is an array of collection info objects that look like:
+//       // { name: 'test', options: {} }
+//   //});
+//   var elements = [];
+//   db.collection("b1", function(err, collection) {
+//     collection.find({"out": "x"}).toArray(function(err, result) {
+//       res.render('index.ejs', {bay: result});
+//     });
+//   });
+//   db.collection("b2", function(err, collection) {
+//     collection.find({"out": "x"}).toArray(function(err, result) {
+//       res.render('index.ejs', {bay2: result, bay3: result});
+//     });
+//   });
+// //  res.render('index.ejs', {bay: elements});
+// });
